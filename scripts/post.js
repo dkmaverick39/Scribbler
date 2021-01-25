ac491f6f3309068973a4640eb078ec93dd4003a9
dkmaverick39 <deepak.k039@gmail.com>
2021-01-25 17:39:49 +0530

    Commit changes for post , comment and likes

---
+++ scripts/post.js
@@ -0,0 +1,71 @@
+var url_string = window.location.href;
+var url = new URL(url_string);
+var postId = url.searchParams.get("postId");
+
+var allPosts = JSON.parse(localStorage.getItem('allInitialPosts'));
+
+var postContent = '';
+var postHeader = '';
+var postAuthor = '';
+var noOfLikes = 0;
+var postAllComments;
+for(var i=0; i<allPosts.length;i++){
+    if(allPosts[i].id == postId){
+        postContent = allPosts[i].content;
+        postHeader = allPosts[i].header;
+        postAuthor = allPosts[i].name;
+        noOfLikes = allPosts[i].likes;
+        postAllComments =  allPosts[i].allComments;
+    } 
+};
+
+document.getElementById('postHeader').innerHTML = postHeader;
+document.getElementById('postAuthor').innerHTML = postAuthor;
+document.getElementById('postContent').innerHTML = postContent;
+if(noOfLikes > 0){
+   document.getElementById('noOfLikesCount').innerHTML = noOfLikes + ' people likes this!'; 
+}
+
+displayComments(postAllComments);
+
+function incrementLikesCount(){
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var postId = url.searchParams.get("postId");
+    for(var i=0; i<allPosts.length;i++){
+        if(allPosts[i].id == postId){
+           var noOfLikes = allPosts[i].likes;
+           noOfLikes = noOfLikes + 1;
+           allPosts[i].likes = noOfLikes;
+           document.getElementById('noOfLikesCount').innerHTML = allPosts[i].likes + ' people likes this!'; 
+           localStorage.setItem('allInitialPosts' ,JSON.stringify(allPosts));         
+        }
+    };    
+}
+
+function addCommentToPost(){
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var postId = url.searchParams.get("postId");
+    var postAllComments ;
+    for(var i=0; i<allPosts.length;i++){
+        if(allPosts[i].id == postId){
+           var comment =  document.getElementById('commentText').value;
+           allPosts[i].allComments.push(comment);
+           localStorage.setItem('allInitialPosts' ,JSON.stringify(allPosts));
+           postAllComments =   allPosts[i].allComments;
+        }
+    }
+   
+    displayComments(postAllComments);
+   
+}
+
+function displayComments(postAllComments){
+    var comments = "";
+    for(var i=0;i<postAllComments.length;i++){
+        comments += "<div style='border:1px solid grey;padding:10px; border-style:solid;border-width:5px 5px 5px 5px;border-color:lightgrey'>"+postAllComments[i]+"</div>"
+    }     
+    document.getElementById("allComments").innerHTML = comments;
+}
+
--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,9 +1,44 @@
 var allInitialPosts = [
-    {id:1,name:"Srishti Gupta",header:"'let' me be a 'const'(ant), not a 'var'(iable)!",content:"Since JavaScript does not have any type-checking, either of these keywords can be used to declare a variable of any type (datatype) in JavaScript. Though all the three keywords are used for the same purpose, they are different."},
-    {id:2,name:"Colby Fayock",header:"What is linting and how can it save you time?",content:"One of the biggest challenges in software development is time. It’s something we can’t easily get more of, but linting can help us make the most out of the time we have."},
-    {id:3,name:"Yazeed Bzadough",header:"How to Get More Views on Your Tech Blog",content:"If you're a developer with a Twitter account, you've already seen everyone and their cat start a blog, YouTube channel, or Patreon. We all want to become stars, or at the very least, a recognizable name in the industry."},
-    {id:4,name:"Cedd Burge",header:"How to write easily describable code",content:"When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes mental energy, and you run the risk of getting the mapping wrong. Different people will map to different words, which leads to confusion when discussing the code."},
-    {id:5,name:"Srishti Gupta",header:"Everything you should know about 'module' & 'require' in Node.js",content:"Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node, and you can say that you’ve created a module named xyz."}
+    {   id:1,
+        name:"Srishti Gupta",
+        header:"'let' me be a 'const'(ant), not a 'var'(iable)!",
+        shortContent : "Since JavaScript does not have any type-checking, either of these keywords can be used to declare a variable of any type (datatype) in JavaScript. Though all the three keywords",
+        content:"Since JavaScript does not have any type-checking, either of these keywords can be used to declare a variable of any type (datatype) in JavaScript. Though all the three keywords are used for the same purpose, they are different.",
+        likes:0,
+        allComments:[]
+    },
+    {   id:2,
+        name:"Colby Fayock",
+        header:"What is linting and how can it save you time?",
+        shortContent : "One of the biggest challenges in software development is time. It’s something we can’t easily get more",
+        content:"One of the biggest challenges in software development is time. It’s something we can’t easily get more of, but linting can help us make the most out of the time we have.",
+        likes:0,
+        allComments:[]
+    },
+    {   id:3,
+        name:"Yazeed Bzadough",
+        header:"How to Get More Views on Your Tech Blog",
+        shortContent : "If you're a developer with a Twitter account, you've already seen everyone and their cat start a blog, YouTube channel",
+        content:"If you're a developer with a Twitter account, you've already seen everyone and their cat start a blog, YouTube channel, or Patreon. We all want to become stars, or at the very least, a recognizable name in the industry.",
+        likes:0,
+        allComments:[]
+    },
+    {   id:4,
+        name:"Cedd Burge",
+        header:"How to write easily describable code",
+        shortContent : "When code is not describable using words, most people have to do some mental mapping to turn it in to words.",
+        content:"When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes mental energy, and you run the risk of getting the mapping wrong. Different people will map to different words, which leads to confusion when discussing the code.",
+        likes:0,
+        allComments:[]
+    },
+    {   id:5,
+        name:"Srishti Gupta",
+        header:"Everything you should know about 'module' & 'require' in Node.js",
+        shortContent : "Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this",
+        content:"Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node, and you can say that you’ve created a module named xyz.",
+        likes:0,
+        allComments:[]
+    }
 ];
 
 
@@ -19,7 +54,8 @@ function displayAllPosts(allPosts){
        posthtml += "<p class='card-text'></p>";
        posthtml += "<h4 class='card-title'><span>"+allPosts[i].header+"</span><span onclick='deletePost("+allPosts[i].id+")' style='float:right'><i class='fa fa-trash' aria-hidden='true'></i></span></h4>";
 
-       posthtml += "<p class='card-text'>"+allPosts[i].content+"</p>";
+       posthtml += "<p class='card-text'>"+allPosts[i].shortContent+"</p>";
+       posthtml += "<div style='float:right;cursor:pointer' onclick='redirectToPostDetails("+allPosts[i].id+")'>. . .</div>";
        posthtml += "</div></div>";
   }
   document.getElementById("allPosts").innerHTML = posthtml;
@@ -43,4 +79,9 @@ function deletePost(postId){
       //alert(posts);
       localStorage.setItem('allInitialPosts' ,JSON.stringify(posts));
       displayAllPosts(posts);
+}
+
+
+function redirectToPostDetails(postId){
+    window.location.href="../html/post.html?postId="+postId
 }
